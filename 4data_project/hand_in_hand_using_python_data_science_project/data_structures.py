45bc6a9823333bad5e703f045ac4a35fda26a187
wangluqing <wangluqing360@163.com>
2018-02-06 21:20:57 +0800

    Python语言

---
+++ 4data_project/hand_in_hand_using_python_data_science_project/data_structures.py
@@ -0,0 +1,26 @@
+list1 = []
+print(list1)
+list2 = [1,]
+print(list2)
+list3 = [1, 2]
+print(list3)
+
+tuple1 = (1, 2, 3)
+print(tuple1)
+print(tuple1[0])
+
+dic1 = {'book1':'C语言','book2':'R语言', 'book3':'Python语言'}
+print(dic1)
+dic1['book4'] = '深度学习与keras'
+print(dic1)
+print(dic1.keys())
+print(dic1.values())
+
+# 字符串
+"""
+座右铭：好好学习，天天向上
+"""
+str1 = '好好学习'
+print(str1)
+str2 = "天天向上"
+print(str2)
